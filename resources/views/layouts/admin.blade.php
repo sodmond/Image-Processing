d870af58b296abaeb403ed3cd39380a902bf3f61
Sodik Owolabi <siresodmond@gmail.com>
2023-09-27 15:35:17 +0100

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -116,7 +116,7 @@
                     </button>
 
                     <!-- Topbar Search -->
-                    <form action="{{ '' }}" method="get"
+                    <form action="{{ route('user.images') }}" method="get"
                         class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                         <div class="input-group">
                             <input type="text" class="form-control bg-light border-0 small" placeholder="Search for image records with name"
