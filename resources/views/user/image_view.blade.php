889414e80795577828b5c75aa3f60cd8d70daaee
Sodik Owolabi <siresodmond@gmail.com>
2023-11-01 19:55:58 +0100

    Final Review Update

--- resources/views/user/image_view.blade.php
+++ resources/views/user/image_view.blade.php
@@ -26,22 +26,22 @@
     </head>
     <body>
         <div style="width:700px; height:990px; background:url('{{ asset("work/resized/".$imageInfo['original_image'])}}'); background-repeat:no-repeat; background-size:cover; background-position: center;">
-            <div style="position:absolute; margin-left:305px; margin-top:10px;"><img src="{{ asset('img/logo.png') }}" width="75"></div>
-            <div style="width:inherit; position:absolute; margin-top:80px; text-align:center;">
-                <img src="{{ asset('img/originally-black0.png') }}" style="width:450px;">
+            <!--<div style="position:absolute; margin-left:305px; margin-top:10px;"><img src="{{ asset('img/logo.png') }}" width="75"></div>-->
+            <div style="width:inherit; position:absolute; margin-top:20px; text-align:center;">
+                <img src="{{ asset('img/originally-black0.png') }}" style="width:600px; height:auto;">
             </div>
     
             <!-- Nickname -->
-            <div style="position:absolute; width:160px; margin-top:370px; margin-left:530px; text-align:right; font-size:100%;">
-                <div style="color:#FFF; font-size:15px; font-family:PlayfairDisplay; font-weight:400; text-transform:uppercase;">Nickname</div>
+            <div style="position:absolute; width:220px; margin-top:280px; margin-left:480px; text-align:center; font-size:100%;">
+                <div style="color:#FFF; font-size:18px; font-family:PlayfairDisplay; font-weight:600; text-transform:uppercase;">Nickname</div>
                 @php $nickname = explode(' ', $imageInfo['nickname']) @endphp
-                <span style="color:#E1CF93; padding-right:10px; font-family:Barabara; text-transform:uppercase;">@foreach($nickname as $nick) {{ $nick }} @endforeach</span>
+                <span style="color:#E1CF93; padding-right:10px; font-family:Barabara; text-transform:uppercase;">@foreach($nickname as $nick) {{ $nick }}<br> @endforeach</span>
             </div>
     
             <!-- Traits -->
-            <div style="position:absolute; width:inherit; margin-top:350px;">
-                <div style="color:#E1CF93; font-size:23px; padding-left:20px; font-family:Barabara; text-transform:uppercase;">Originality <br>Traits</div>
-                <ul style="color:#FFF; margin-top:0; font-family:Arial, Helvetica, sans-serif; font-size:13px">
+            <div style="position:absolute; width:inherit; margin-top:370px; margin-left:10px;">
+                <div style="color:#E1CF93; font-size:27px; padding-left:20px; font-family:Barabara; text-transform:uppercase;">Originality <br>Traits</div>
+                <ul style="color:#FFF; margin-top:0; font-family:Arial, Helvetica, sans-serif; font-size:17px; font-weight:600;">
                     @php 
                         $traits = explode(',', $imageInfo['traits']);
                         for ($i=0; $i < 3; $i++) {
@@ -65,14 +65,15 @@
 	    		<img src="{{ asset('img/barcode.png') }}" style="width:90px; margin-right:50px;">
 		    </div>
 
-            <!-- Drink Responsibly -->
-		    <div style="position:absolute; width:inherit; margin-top:950px;">
-	    		<img src="{{ asset('img/drink-responsibly.png') }}" style="width:90px; margin-left:30px;">
+            <!-- Powered By -->
+		    <div style="position:absolute; width:inherit; margin-top:860px;">
+                <p style="padding-left: 20px;"><span style="color:#FFF; font-size:14px;">Powered by</span><br>
+                <img src="{{ asset('img/logo.png') }}" width="75"></p>
     		</div>
 
-		    <!-- Website link -->
-    		<div style="position:absolute; width:inherit; margin-top:940px; text-align:center; color:#FFF; font-size:14px; font-family: Arial;">
-                Powered by Trophy Stout
+		    <!-- Drink Responsibly -->
+    		<div style="position:absolute; width:300px; margin-top:950px; margin-left:200px; text-align:center; color:#FFF; font-size:14px; font-family: Arial;">
+                <img src="{{ asset('img/drink-responsibly.png') }}" style="width:120px; margin-left:30px;">
 	    	</div>
         </div>
     </body>
